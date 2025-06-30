3081c18c9d1e3787cb11c3fcdd6d2df0cfaf4f6f
shadow <darkshadowblaze786@g.mail.com>
2025-06-30 16:50:24 +0600

    bmi funcyion

--- function.js
+++ function.js
@@ -41,3 +41,18 @@ function findAverage(array) {
     return sum / array.length
 }
 console.log(findAverage([1,2,3,4]))
+
+
+function bmi(weight, height) {
+    let bmi = weight / (height ** 2);
+    if (bmi<= 18.5){
+  return "Underweight";
+}else if(bmi <= 25.0 ){
+    return"Normal"
+}else if(bmi <= 30.0){
+    return"Overweight"
+}else{
+    return"Obese"
+}
+}
+console.log(bmi(80 ,1.80))
