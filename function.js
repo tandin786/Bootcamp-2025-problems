0a460de8e3d07de5f717ebf0e72a7ef994b38606
shadow <darkshadowblaze786@g.mail.com>
2025-06-30 12:45:21 +0600

    save4

--- function.js
+++ function.js
@@ -1,9 +1,21 @@
 function reverseString(string){
     return string.split('').reverse().join('')
 }
-// console.log(reverseString("hello"))
+console.log(reverseString("hello"))
 
 function replace(str, charToReplace, StrReplacement){
      return str.replace(charToReplace, StrReplacement)
 }
 console.log(replace("hello", "o", "p"))
+
+
+function strlengthEven(str){
+    if(str.length % 2 === 0 ){
+        return "even"
+    }else{
+        return "odd"
+    }
+}
+console.log(strlengthEven("hello"))
+
+
