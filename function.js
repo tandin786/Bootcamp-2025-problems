ce1100d98cffb4ccc04bddd501672fa6043318d2
shadow <darkshadowblaze786@g.mail.com>
2025-07-02 10:48:38 +0600

    rps function

--- function.js
+++ function.js
@@ -123,3 +123,18 @@ function XO(str) {
   return Xcount===Ocount
 }
 console.log(XO("xox"))
+
+const rps = (p1, p2) => {
+  if(
+    (p1 === "scissors" &&  p2 === "paper") ||
+    (p1 === "paper" && p2 === "rock") ||
+    (p1 === "rock" && p2 === "scissors")
+){
+    return "Player 1 won!"
+  }else if(p1 === p2){
+    return "Draw!"
+  }else{
+    return "Player 2 won!"
+  }
+};
+console.log(rps("paper", "scissors"))
